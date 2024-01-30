53ed7dbccf80554002323d333f8bd69054eab1e7
“NotScorpio91” <notscorpio91@gmail.com>
2024-01-30 13:00:38 +0500

    Error message added

--- src/components/CardForm.jsx
+++ src/components/CardForm.jsx
@@ -6,7 +6,7 @@ import { deleteAllCards } from '../redux/features/cardsSlice';
 const CardForm = () => {
   const dispatch = useDispatch();
   const cards = useSelector(state => state.cards);
-
+  const [errorMessage, setErrorMessage] = useState('');
   const [formData, setFormData] = useState({
     clientName: '',
     title: '',
@@ -53,8 +53,10 @@ const CardForm = () => {
         image: '',
       });
     } else {
-
-      console.error('All fields are required');
+      setErrorMessage('All fields are required');
+      setTimeout(() => {
+        setErrorMessage('');
+      }, 2000);
     }
   };
 
@@ -71,6 +73,18 @@ const CardForm = () => {
 
   return (
     <div className=" px-4">
+      {errorMessage && (
+        <div class="flex items-center p-4 mb-4 text-sm text-blue-800 rounded-lg bg-blue-50 dark:bg-gray-800 dark:text-blue-400" role="alert">
+        <svg class="flex-shrink-0 inline w-4 h-4 me-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
+          <path d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5ZM9.5 4a1.5 1.5 0 1 1 0 3 1.5 1.5 0 0 1 0-3ZM12 15H8a1 1 0 0 1 0-2h1v-3H8a1 1 0 0 1 0-2h2a1 1 0 0 1 1 1v4h1a1 1 0 0 1 0 2Z"/>
+        </svg>
+        <span class="sr-only">Info</span>
+        <div>
+          <span class="font-medium">Info alert!</span> {errorMessage}
+        </div>
+      </div>
+        
+      )}
       <div className='mb-4 flex flex-col md:flex-row md:items-center space-y-4 md:space-y-0 md:space-x-4'>
       <div className="flex-grow md:w-1/6">
         <label className="block text-black mb-1">Client Name</label>
