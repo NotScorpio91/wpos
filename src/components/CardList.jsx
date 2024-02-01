5e5120e93401ef5a2c2d356b313c1f6f0a19fd3e
“NotScorpio91” <notscorpio91@gmail.com>
2024-02-01 15:36:35 +0500

    data

--- src/components/CardList.jsx
+++ src/components/CardList.jsx
@@ -1,11 +1,16 @@
 import { useSelector, useDispatch } from 'react-redux';
 import { addCard, deleteCard, deleteAllCards } from '../redux/features/cardsSlice';
 import Card from './Card';
+import { moveCardToSecCJ } from '../redux/features/cardsSlice';
 import { useEffect } from 'react';  
 
 const CardList = () => {
   const cards = useSelector(state => state.cards);
   const dispatch = useDispatch();
+
+  const handleJobDone = card => {
+    dispatch(moveCardToSecCJ(card));
+  };
 
   useEffect(() => {
     const storedCards = JSON.parse(localStorage.getItem('cards')) || [];
@@ -18,9 +23,9 @@ const CardList = () => {
   };
 
   return (
-    <div className='grid sm:grid-cols- px-4  '>
+     <div className='grid sm:grid-cols- px-4  '>
       {cards.map(card => (
-        <Card key={card.id} card={card} onDelete={() => handleDelete(card)} />
+        <Card key={card.id} card={card} onDelete={() => handleDelete(card)} onJobDone={() => handleJobDone(card)} />
       ))}
     </div>
   );
--- src/redux/features/cardsSlice.js
+++ src/redux/features/cardsSlice.js
@@ -1,3 +1,5 @@
+// cardsSlice.js
+
 import { createSlice } from '@reduxjs/toolkit';
 
 const initialState = JSON.parse(localStorage.getItem('cards')) || [];
@@ -19,9 +21,20 @@ export const cardsSlice = createSlice({
       localStorage.removeItem('cards');
       return [];
     },
-  },
+    moveCardToSecCJ: (state, action) => {
+      const cardIndex = state.findIndex(card => card.id === action.payload.id);
+      if (cardIndex !== -1) {
+        const movedCard = state[cardIndex];
+        movedCard.status = 'sec_cj';
+        state.splice(cardIndex, 1);
+        state.push(movedCard);
+        localStorage.setItem('cards', JSON.stringify(state));
+        // console.log('Updated state:', state); // Add this line for debugging
+      }
+    },
+  }
 });
 
-export const { addCard, deleteCard, deleteAllCards } = cardsSlice.actions;
+export const { addCard, deleteCard, deleteAllCards, moveCardToSecCJ } = cardsSlice.actions;
 
 export default cardsSlice.reducer;
