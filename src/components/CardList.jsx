31b10a740a1a3311c6be7c3d35313fdfd4e1591d
“NotScorpio91” <notscorpio91@gmail.com>
2024-02-12 18:33:02 +0500

    done

--- src/components/CardList.jsx
+++ src/components/CardList.jsx
@@ -1,5 +1,7 @@
-import React,{useState,useEffect} from 'react';
+import React from 'react';
 import Card from './Card';
+import { useDispatch } from 'react-redux';
+import { deleteCard } from '../redux/slices/cardsSlice';
 import { db} from "../config/firebase";
 import {
   deleteDoc,
@@ -9,19 +11,27 @@ import {
 
 
 const CardList = ({list}) => {
-  
-  const moviesCollectionRef = collection(db, "cards");
 
-  const deleteCard = async (id) => {
+  const dispatch = useDispatch();
+
+  const Delete = card => {
+    dispatch(deleteCard(card));
+  };
+  
+  const collectionRef = collection(db, "cards");
+
+  const handleDelete = async (id) => {
     const cardDoc = doc(db, "cards", id);
     await deleteDoc(cardDoc);
   };
+
+  
 
 
   return (
     <div className='grid sm:grid-cols-2 sm:space-x-4 md:flex-col md:grid-cols-2 xl:grid-cols-3 2xl:grid-cols-4 px-4 justify-center items-center '>
       {list?.map(card => (
-        <Card key={card.id} card={card}  onDelete={() => deleteCard(card.id)} />
+        <Card key={card.id} card={card}  onDelete={() => handleDelete(card.id)} />
       ))}
     
     </div>
