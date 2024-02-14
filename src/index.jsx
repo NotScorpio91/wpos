f204c32d40c94cdb86fd4153cf498e97dddf13a2
“NotScorpio91” <notscorpio91@gmail.com>
2024-02-14 12:28:49 +0500

    Pagenotfound added

--- src/index.jsx
+++ src/index.jsx
@@ -1,3 +1,5 @@
 export { default as Home } from './Home';
 export { default as HomePage } from './Pages/HomePage';
 export { default as CardForm } from './Pages/CardForm';
+export { default as NotFoundPage } from './Pages/NotFoundPage';
+
